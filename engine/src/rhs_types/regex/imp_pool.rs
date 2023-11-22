410b6fea3e8515f1f5ea996d32d3755661d38832
Eliad Peller <eliad.peller@wiz.io>
2023-11-22 20:00:05 +0200

    regex_pool: some improvements
    Signed-off-by: Eliad Peller <eliad.peller@wiz.io>

--- engine/src/rhs_types/regex/imp_pool.rs
+++ engine/src/rhs_types/regex/imp_pool.rs
@@ -1,5 +1,6 @@
 use hashbrown::HashSet;
 use lazy_static::lazy_static;
+use std::borrow::Borrow;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
@@ -15,22 +16,18 @@ lazy_static! {
 
 impl Drop for Regex {
     fn drop(&mut self) {
-        // The logic here is a bit hacky, we check the strong_count for 2,
-        // because we have this reference and the one that lives in the HashSet.
-        // Once we will call remove on the pool, we will enter this function again
-        // this time with the entry that came from the HashSet, it too has a strong_count
-        // of 2, because we have not finished dropping the previous Arc.
-        // In order to distinguish between the two, we are doing a small hack here and
-        // take a weak reference prior to calling remove, this way the 2nd drop can know
-        // it's the one that came from the pool and does not need to do any additional
-        // work.
-        // This is how we solve the deadlock of the mutex being acquired more than once.
-        if Arc::strong_count(&self.0) == 2 && Arc::weak_count(&self.0) == 0 {
-            let _dummy_weak = Arc::downgrade(&self.0);
+        // check whether this is the last strong reference to the regex, and
+        // avoid deadlock by making sure to drop the last cached regex only
+        // after we've dropped the lock on the pool.
+        let cached_regex = if Arc::strong_count(&self.0) == 2 && Arc::weak_count(&self.0) == 0 {
             let mut pool = REGEX_POOL.lock().unwrap();
-            pool.remove(&self);
-            return;
-        }
+            pool.take(self.as_str())
+        } else {
+            None
+        };
+
+        // now we can safely drop the regex, as there's no deadlock
+        drop(cached_regex);
     }
 }
 
@@ -38,14 +35,19 @@ impl FromStr for Regex {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Error> {
+        let mut pool = REGEX_POOL.lock().unwrap();
+        if let Some(regex) = pool.get(s) {
+            return Ok(regex.clone());
+        }
+
         let regex = Self(Arc::new(
             ::regex::bytes::RegexBuilder::new(s)
                 .unicode(false)
                 .build()?,
         ));
 
-        let mut pool = REGEX_POOL.lock().unwrap();
-        Ok(pool.get_or_insert(regex).clone())
+        pool.insert(regex.clone());
+        Ok(regex)
     }
 }
 
@@ -57,6 +59,12 @@ impl Regex {
 
     /// Returns the original string of this regex.
     pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
+impl Borrow<str> for Regex {
+    fn borrow(&self) -> &str {
         self.0.as_str()
     }
 }
